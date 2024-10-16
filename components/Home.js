9add270228c71e6495a6fe66fd57c43e2be1bcd2
namanpunn <namanpunn3@gmail.com>
2024-10-16 18:27:35 +0530

    2

--- components/Home.js
+++ components/Home.js
@@ -1,17 +1,16 @@
-import React from 'react'
-import Image from 'next/image'
+import Image from "next/image"
 
-const Home = () => {
+const page = () => {
   return (
-    <div>
-      <section className="flex flex-col h-screen p-4 justify-center">
+    <>
+      <section className="flex flex-col items-center justify-between h-screen p-6">
         {/* Skip button */}
-        <div className="flex justify-end w-full mb-4">
+        <div className="flex justify-end w-full pt-5 p-3">
           <p className="font-semibold">Skip</p>
         </div>
 
         {/* Image */}
-        <div className="flex justify-center mb-4">
+        <div className="flex justify-center items-center my-6">
           <Image 
             src="/images/bus.jpg" 
             alt="School bus" 
@@ -21,27 +20,25 @@ const Home = () => {
         </div>
 
         {/* Heading and description */}
-        <div className="text-center mb-4">
+        <div className="text-center">
           <h1 className="text-lg font-semibold">We Provide School Bus GPS Tracking System</h1>
-          <p className="text-gray-500">Tracking your child during travel in the school bus</p>
+          <p className="text-gray-500">Tracking your child during traveling in school bus</p>
         </div>
 
         {/* Pagination dots */}
-        <div className="flex justify-center mb-4">
+        <div className="flex justify-center my-3">
           <span className="h-2 w-2 bg-gray-300 rounded-full mx-1"></span>
           <span className="h-2 w-2 bg-blue-500 rounded-full mx-1"></span>
           <span className="h-2 w-2 bg-gray-300 rounded-full mx-1"></span>
         </div>
 
         {/* Get Started button */}
-        <div className="flex justify-center">
-          <button className="bg-blue-600 text-white text-lg py-2 px-10 rounded-full">
-            Get Started
-          </button>
-        </div>
+        <button className="bg-blue-600 text-white text-lg py-2 px-10 rounded-full mt-4">
+          Get Started
+        </button>
       </section>
-    </div>
+    </>
   )
 }
 
-export default Home
+export default page
